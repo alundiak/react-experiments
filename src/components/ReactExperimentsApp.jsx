9b795c144e404e5ce2a1083fe210dd202ec35c05
Andrii Lundiak <landike@gmail.com>
2019-04-21 15:18:56 +0200

    Upgraded to ReactSum v1.2.0

--- src/components/ReactExperimentsApp.jsx
+++ src/components/ReactExperimentsApp.jsx
@@ -10,7 +10,11 @@ import ReactSum from '@lundiak/react-sum';
 // Using { } for custom import from the same App.jsx will still import all less file imports.
 //
 import { Sum } from '@lundiak/react-sum';
-import { Count } from '@lundiak/react-sum';
+import { SumWithCssAlias } from '@lundiak/react-sum';
+
+//
+// From SRC
+//
 
 //
 // If App.jsx exports Sum.jsx and Count.jsx with their less files,
@@ -18,44 +22,49 @@ import { Count } from '@lundiak/react-sum';
 // Solution - need to import directly.
 //
 // import Sum from '@lundiak/react-sum/src/components/Sum';
-// import Count from '@lundiak/react-sum/src/components/Count';
-import CountWithAlias from '@lundiak/react-sum/src/components/CountWithAlias'; // works
-// import CountWithAlias from '@lundiak/react-sum/dist/CountWithAlias'; // doesn't work
+// import SumWithCssAlias from '@lundiak/react-sum/src/components/SumWithCssAlias';
+
+//
+// From DIST
+//
+
+import DistSum from '@lundiak/react-sum/dist/Sum'; // works (after change devBuild with optimization to prodBuild w o.)
+import DistSumWithCssAlias from '@lundiak/react-sum/dist/SumWithCssAlias';
+
+// Approach with "css" alias doesn't work if LESS injected as dep. inside of entry and SumWithCssAlias.jsx file is npt exposed from App.jsx
+// => "react-dom.development.js:55 Uncaught Invariant Violation: Element type is invalid"
+// still doesn't work.
 
 //
 // Alternative approach, in case of webpack aliasing DIRECTLY to node_modules.
 //
-// import WebpackReactSum from 'reactMath/Sum';
-// import WebpackReactCount from 'reactMath/Count';
-
-//
-// DIST
-//
-import DistCount from '@lundiak/react-sum/dist/Count'; // works (after change devBuild with optimization to prodBuild w o.)
-
-// import DistCount from '@lundiak/react-sum/dist/CountWithAlias';
-// doesn't work after LESS injected as dep. inside of entry
-// => "react-dom.development.js:55 Uncaught Invariant Violation: Element type is invalid"
-// still doesn't work.
+import WebpackSum from 'reactMath/Sum';
+import WebpackSumWithCssAlias from 'reactMath/SumWithCssAlias';
 
 import 'myCss/react-experiments-app.less';
 
-console.log(DistCount); // => ƒ (e) { var t = e.list; return o.a.createElement("div",
+// console.log(ReactSum); // {Sum: ƒ, SumWithCssAlias: ƒ}
+// console.log(Sum); // ƒ Sum() {
+// console.log(SumWithCssAlias); // ƒ SumWithCssAlias() {
+// console.log(DistSum); // function ƒ t() {
+// console.log(DistSumWithCssAlias); // function ƒ t() {
+// console.log(WebpackSum); // function ƒ Sum() {
+// console.log(WebpackSumWithCssAlias); // function ƒ SumWithCssAlias() {
 
 const ReactExperimentsApp = (/* props */) => (
     <React.Fragment>
         {/* <ReactColorSquare /> */}
         <ReactSum.Sum />
-        <ReactSum.Count />
+        <ReactSum.SumWithCssAlias />
 
         <Sum a={3} b={3} />
-        <Count list={[1, 2, 3]} />
+        <SumWithCssAlias a={3} b={3} />
 
-        <DistCount list={[1, 2, 3, 4]} />
+        <DistSum a={4} b={4} />
+        <DistSumWithCssAlias a={5} b={5} />
 
-        <CountWithAlias list={[1, 2, 3, 4, 5]} />
-        {/* <WebpackReactSum /> */}
-        {/* <WebpackReactCount /> */}
+        <WebpackSum a={6} b={6} />
+        <WebpackSumWithCssAlias a={6} b={6} />
     </React.Fragment>
 );
 
