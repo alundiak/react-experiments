f4c314070bf7589cda412ceeb32d2845b4229e36
Andrii Lundiak <landike@gmail.com>
2019-05-15 21:35:51 +0200

    Introduced useReduser example.

---
+++ src/components/MyUseReducer.jsx
@@ -0,0 +1,94 @@
+// https://www.robinwieruch.de/react-usereducer-hook/
+// Reworked, Modified, Adapted.
+import React from 'react';
+
+const initialTodos = [
+    {
+        id: 'a',
+        task: 'Learn React',
+        complete: false,
+    },
+    {
+        id: 'b',
+        task: 'Learn Firebase',
+        complete: false,
+    },
+];
+
+const AppSimple = () => {
+    const handleChange = () => { };
+
+    return (
+        <ul>
+            {initialTodos.map(todo => (
+                <li key={todo.id}>
+                    <label htmlFor={`test-${todo.id}`}>
+                        <input
+                            id={`test-${todo.id}`}
+                            type="checkbox"
+                            checked={todo.complete}
+                            onChange={handleChange}
+                        />
+                        {todo.task}
+                    </label>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        case 'DO_TODO':
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return { ...todo, complete: true };
+                }
+                return todo;
+            });
+        case 'UNDO_TODO':
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    return { ...todo, complete: false };
+                }
+                return todo;
+            });
+        default:
+            return state;
+    }
+};
+
+const AppWithReducer = () => {
+    const [todos, dispatch] = React.useReducer(
+        todoReducer,
+        initialTodos
+    );
+
+    const handleChange = todo => dispatch({
+        type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+        id: todo.id
+    });
+
+    return (
+        <ul>
+            {todos.map(todo => (
+                <li key={todo.id}>
+                    <label htmlFor={`test-${todo.id}`}>
+                        <input
+                            id={`test-${todo.id}`}
+                            type="checkbox"
+                            checked={todo.complete}
+                            onChange={() => handleChange(todo)}
+                        />
+                        {todo.task}
+                    </label>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default {
+    AppSimple,
+    AppWithReducer
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactExperimentsApp from 'components/ReactExperimentsApp';
+// import ReactExperimentsApp from 'components/ReactExperimentsApp';
+import MyUseReducer from 'components/MyUseReducer';
 
-ReactDOM.render(<ReactExperimentsApp />, document.getElementById('reactExperiments'));
+ReactDOM.render(<MyUseReducer.AppWithReducer />, document.getElementById('reactExperiments'));
 
 if (module.hot) {
   module.hot.accept();
