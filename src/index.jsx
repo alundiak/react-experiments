ace740b0b448067455a48df40ee8a9ec5022e765
Andrii Lundiak <landike@gmail.com>
2019-06-16 18:10:32 +0200

    Modified index. Upgrade deps.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ReactExperimentsApp from 'components/ReactExperimentsApp';
-import MyUseReducer from 'components/MyUseReducer';
+import ReactExperimentsApp from 'components/ReactExperimentsApp';
+// import MyUseReducer from 'components/MyUseReducer';
 
-ReactDOM.render(<MyUseReducer.AppWithReducer />, document.getElementById('reactExperiments'));
+ReactDOM.render(<ReactExperimentsApp />, document.getElementById('reactExperiments'));
+// ReactDOM.render(<MyUseReducer.AppWithReducer />, document.getElementById('reactExperiments'));
 
 if (module.hot) {
   module.hot.accept();
